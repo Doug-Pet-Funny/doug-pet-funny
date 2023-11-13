2de76e8607cff254f76018cbf2206532117c30b9
vitorbizarra <vitorbizarracontato@gmail.com>
2023-11-13 14:44:59 -0300

    :wrench: chore: OrderSeeder e OrderFactory

--- database/factories/Orders/OrderFactory.php
+++ database/factories/Orders/OrderFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories\Orders;
 
+use App\Enums\OrderStatusEnum;
+use App\Enums\PaymentMethodsEnum;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,21 @@ class OrderFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'status' => fake()->randomElement(OrderStatusEnum::class),
+            'payment_method' => fake()->randomElement(PaymentMethodsEnum::class),
+            'total' => fake()->randomNumber(6, true),
+            'items' => [
+                [
+                    'pet' => fake()->firstName(),
+                    'item' => fake()->words(asText: true),
+                    'price' => fake()->randomNumber(6, true),
+                    'employee' => fake()->name(),
+                    'quantity' => 1
+                ],
+            ],
+            'date' => fake()->date(),
+            'start_hour' => fake()->time(),
+            'end_hour' => fake()->time(),
         ];
     }
 }
--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customers\Customer;
+use App\Models\Orders\Order;
+use App\Models\Services\Employee;
+use App\Models\Services\Service;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Seeder;
 
 class OrderSeeder extends Seeder
@@ -12,6 +17,24 @@ class OrderSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Order::factory(10)->create()->each(function (Order $order) {
+            $customer = Customer::inRandomOrder()->limit(1)->first();
+            $order->customer()->associate($customer);
+
+            $service = Service::inRandomOrder()->limit(1)->first();
+            $employee = Employee::whereHas('services', fn (Builder $query) => $query->where('name', $service->name))->get()->first();
+            $order->items = [
+                [
+                    'pet' => $customer->pets->first()->name,
+                    'item' => $service->name,
+                    'price' => $service->price,
+                    'employee' => $employee->name,
+                    'quantity' => 1
+                ]
+            ];
+            $order->total = $service->price;
+
+            $order->save();
+        });
     }
 }
